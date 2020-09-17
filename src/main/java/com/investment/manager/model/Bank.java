e4550ddad07342d621a51eb400118fa78f0e2ffc
eduardocampigoto <eduardo.campigoto@gmail.com>
2020-09-16 21:16:18 -0300

    Removed AUTO_GENERATE from bank entity ID

--- src/main/java/com/investment/manager/model/Bank.java
+++ src/main/java/com/investment/manager/model/Bank.java
@@ -1,8 +1,6 @@
 package com.investment.manager.model;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import org.hibernate.annotations.DynamicUpdate;
@@ -21,7 +19,6 @@ import lombok.Setter;
 public class Bank {
 
 	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long code;
 	
     private String name;
